f44ef239a0278cb582bb64335a33d34dfa4b91ec
Vitaly <kentanohin478@gmail.com>
2022-06-25 17:57:00 +0300

    Example005

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.Write("Введите имя пользователя: ");
+ string username = Console.ReadLine();
+
+if(username.ToLower() == "диана")
+{
+    Console.WriteLine("Ура, это же ДИАНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
